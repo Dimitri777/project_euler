203e9b7070aefb38a8bf548f839514b9bc577a7c
Dimitri777 <54778099+Dimitri777@users.noreply.github.com>
2025-08-02 15:18:23 +0300

    euler_3.py
    Problem 3 description added

--- euler_3.py
+++ euler_3.py
@@ -1,6 +1,6 @@
 # https://projecteuler.net/problem=3
 """
-
+The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number600851475143?
 """
 
 def largest_prime_factor(n):
